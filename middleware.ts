60f51dc3f4bb7a6338e453a62040d8bb353676a5
darlingson <dmakuwila@gmail.com>
2024-06-06 19:21:19 +0200

    created the middleware file

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('token')?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET as string);
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+}
+
+export const config = {
+  matcher: ['/lesson-plans/:path*'],
+};
